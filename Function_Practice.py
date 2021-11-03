97a7dc692ea97e85096224af4701505f69169070
SpaceBanana896 <22mattp@wps39.org>
2021-11-03 20:09:31 +0000

    stated fork

--- Function_Practice.py
+++ Function_Practice.py
@@ -1,1 +1,12 @@
+import random
+import time 
 
+def roadFork():
+    fork = ''
+
+    while fork != '1' and fork 1= '2':
+        print("Please choose a direction to go (1 or 2)")
+        fork = input()
+    return fork
+
+def 
